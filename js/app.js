84c2ed9852b0dba8d4a5d112716d04283025699b
cat-mem <115585790+cat-mem@users.noreply.github.com>
2023-06-14 08:47:46 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
 gsap.registerPlugin(ScrollTrigger, ScrollSmoother)
+
+gsap.fromTo('.hero__content', { backgroundPosition: '1px 0' }, {
+	backgroundPosition: 'calc(var(--index)*20) 0', scrollTrigger: {
+			trigger: '.hero__content',
+			scrub: true
+	}
+})
+
+gsap.fromTo('.rotateImage', { rotation: 0 }, {
+	rotation: 360, scrollTrigger: {
+			trigger: 'rotateImage',
+			scrub: true
+	}
+})
+
